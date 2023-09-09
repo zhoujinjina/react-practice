4bf3081d698cb4f9b8aa249a9254d9575b643693
zhoujinjina <3513776811@qq.com>
2023-09-09 19:44:10 +0800

    完成轮播图组件

--- vite-project/src/component/banner/Banner.jsx
+++ vite-project/src/component/banner/Banner.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
-import "./Banner.css"
+import React, { lazy, useEffect, useRef } from "react";
+import "./Banner.scss";
 const Banner = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const ref = useRef();
+let timeout
+  const to = (dir) => {
+if(timeout)return
+timeout=setTimeout(()=>{
+  
+  let dom = ref.current;
+  let scrollMount =
+    dir == "left"
+      ? dom.scrollLeft - dom.offsetWidth
+      : dom.scrollLeft + dom.offsetWidth;
+  if (dom.scrollLeft == dom.offsetWidth * 4) {
+    dom.scrollTo({
+      left: 0,
+      behavior: "smooth",
+    });
+  } else {
+    dom.scrollTo({
+      left: scrollMount,
+      behavior: "smooth",
+    });
+  }
+ setTimeout(()=>timeout=null,800)
+},0)
 
-export default Banner
+
+  };
+  return (
+    <>
+      <div className="banner" ref={ref}>
+        <div className="include">
+          <img
+            src="https://img1.baidu.com/it/u=2880053401,2868448360&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=500"
+            alt="懒洋洋"
+            title="this is LanYangYang"
+          />
+        </div>
+        <div className="include">
+          <img
+            src="https://img1.baidu.com/it/u=2880053401,2868448360&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=500"
+            alt="懒洋洋"
+            title="this is LanYangYang"
+          />
+        </div>
+        <div className="include">
+          <img
+            src="https://img1.baidu.com/it/u=2880053401,2868448360&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=500"
+            alt="懒洋洋"
+            title="this is LanYangYang"
+          />
+        </div>
+        <div className="include">
+          <img
+            src="https://img1.baidu.com/it/u=2880053401,2868448360&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=500"
+            alt="懒洋洋"
+            title="this is LanYangYang"
+          />
+        </div>
+        <div className="include">
+          <img
+            src="https://img1.baidu.com/it/u=2880053401,2868448360&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=500"
+            alt="懒洋洋"
+            title="this is LanYangYang"
+          />
+        </div>
+      </div>
+      <button onClick={() => to("left")}>left</button>
+      <button onClick={() => to("right")}>right</button>
+    </>
+  );
+};
+
+export default Banner;
