441941681c5b0292e35ffdf494bb0921a69b1f65
zhoujinjina <3513776811@qq.com>
2023-09-09 16:25:12 +0800

    initial

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Banner from "./component/banner/Banner";
+import Scroll from "./component/scroll/Scroll";
+
+function App() {
+  return (
+    <>
+      <Scroll />
+      <Banner />
+    </>
+  );
+}
+
+export default App;
